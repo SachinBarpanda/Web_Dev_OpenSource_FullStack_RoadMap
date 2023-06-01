f867833de202db0d57eb210e923211b607d51902
Sachin <sachinongithub@gmail.com>
2023-06-01 10:59:35 +0530

    Lec 42 codes updated

---
+++ Lec42/expressBasic/index.js
@@ -0,0 +1,12 @@
+const express = require('express');//function 
+const app = express();
+
+// console.log(app);
+
+// ask server to listen to your request
+app.listen(8080,()=>{
+    console.log('Server running on port 8080')
+})
+
+
+
